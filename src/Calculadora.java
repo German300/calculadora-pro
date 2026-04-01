103f17d486f2b25ce05236b2c7b24fa92337260e
leandro raul vallejo <vallejoleandro470@gmail.com>
2026-04-01 19:48:09 -0300

    cree la division

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,10 +1,11 @@
 public class Calculadora {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 15;
+        int a = 4;
+        int b = 2;
         System.out.println("Suma: " + (a + b));
         System.out.println("Resta: " + (a - b));
         System.out.println("multiplicacion"+ (a * b));
+        System.out.println("multiplicacion"+ (a / b));
 
     }
 }

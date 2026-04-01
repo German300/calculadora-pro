fe03a0e77548fc95727bd67ae4c0528515af840a
leandro raul vallejo <vallejoleandro470@gmail.com>
2026-04-01 19:40:03 -0300

    ya hice la multiplicacion

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,9 @@
 public class Calculadora {
     public static void main(String[] args) {
         int a = 10;
-        int b = 10;
+        int b = 15;
         System.out.println("Suma: " + (a + b));
         System.out.println("Resta: " + (a - b));
+        System.out.println("multiplicacion"+ (a * b));
     }
 }
